c17b3dddd6da6992171de09e2e023e154ac08201
kokiecn <73936162+kokiecn@users.noreply.github.com>
2021-03-19 20:40:08 +0900

    Update DTrackAnimation.cs
    modification

--- IKcustom/Assets/scripte/DTrackAnimation.cs
+++ IKcustom/Assets/scripte/DTrackAnimation.cs
@@ -5,18 +5,18 @@ public class DTrackAnimation : MonoBehaviour
 {
 
     private List<Transform> bones = new List<Transform>(); // liste des bones possédant le script DTrackPose
-    private List<Quaternion> rotationOffsets = new List<Quaternion>(); // liste des quaternions offsets pour chaque bone
-    private List<Vector3> positionOffsets = new List<Vector3>(); // liste des positions offsets pour chaque bone
+    //private List<Quaternion> rotationOffsets = new List<Quaternion>(); // liste des quaternions offsets pour chaque bone
+    //private List<Vector3> positionOffsets = new List<Vector3>(); // liste des positions offsets pour chaque bone
     private Transform currentChild;
     public int firstFrame = 10; // frame à laquelle on mesure les offsets
     private float eps = 0.0001f;
     private List<Quaternion> rotationStandards = new List<Quaternion>();
-    private List<Vector3> positionStandards = new List<Vector3>();
+    //private List<Vector3> positionStandards = new List<Vector3>();
 
     // Use this for initialization
     void Start()
     {
-        //AddChildrenToList (this.gameObject.transform);
+       
     }
 
     // Update is called once per frame
@@ -33,14 +33,14 @@ public class DTrackAnimation : MonoBehaviour
             for (int i = 0; i < bones.Count; i++)
             {
                 DTrackPose pose = bones[i].GetComponent<DTrackPose>();
-                Quaternion orientation = pose.body_desc.orientation;
+                //Quaternion orientation = pose.body_desc.orientation;
                 Vector3 position_world = pose.body_desc.position;
 
                 Quaternion quat = rotationStandards[i];
 
                 if (i == 0)
                  {
-                     bones[i].position = position_world;
+                    bones[i].position = position_world;
                     bones[i].transform.rotation = pose.body_desc.orientation * quat;
 
                 }
@@ -48,13 +48,7 @@ public class DTrackAnimation : MonoBehaviour
                  {
                     bones[i].transform.rotation = pose.body_desc.orientation * quat;
                 }
-                 
-                 
-
-
-              
-
-               
+                              
             }
         }
     }
@@ -78,23 +72,14 @@ public class DTrackAnimation : MonoBehaviour
             {
                 
                 bones.Add(currentChild); // on ajoute à la table le transform de l'enfant
-                rotationOffsets.Add(pose.body_desc.orientation);// quaternion orientation world coordonees  // on ajoute la première rotation envoyée par DTrack : ceci sera notre offset pour la suite
+               // rotationOffsets.Add(pose.body_desc.orientation); // on ajoute la première rotation envoyée par DTrack : ceci sera notre offset pour la suite
                 rotationStandards.Add(currentChild.rotation);
-                positionOffsets.Add(pose.body_desc.position);//vector3 position world
-                positionStandards.Add(currentChild.localPosition);// vector3  local position  ce sera pas necessaire je dirais
+              // positionOffsets.Add(pose.body_desc.position);
+               // positionStandards.Add(currentChild.localPosition);// vector3  local position  ce sera pas necessaire je dirais
             }
             AddChildrenToList(currentChild); // on traite les petits enfants
         }
     }
 
-    private void SetTransform(Transform bone, Quaternion Rstandard, Quaternion Roffset, Vector3 Poffset, int i)
-    {
 
-        DTrackPose pose = bone.GetComponent<DTrackPose>();
-
-        Quaternion quat = Quaternion.Inverse(Rstandard);
-
-        bone.transform.rotation = pose.body_desc.orientation;
-        
-    }
 }
